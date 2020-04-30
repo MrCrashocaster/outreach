07efd063b689eec5236012e428426d35da3f15b7
MrCrashocaster <mrcrashocaster@gmail.com>
2020-04-30 00:19:03 -0500

    Updated

--- www.outreach.olemiss.edu/js/quiz.js
+++ www.outreach.olemiss.edu/js/quiz.js
@@ -446,8 +446,7 @@
 		if (currentQuestionIndex === i) {
 			console.error('Attempting to transition to current question');
 			var img1 = "https://www.gwc.org.uk/default/cache/file/4D81E70D-60C4-4FE7-B4DBD4299B06FAD3_fullwidth.jpg";
-			var img2 = "img/major.jpg";
-			
+			var img2 = "img/major.jpg";			
 			var imgElement = document.getElementById('toggleImage');
 		 
 			imgElement.src = (imgElement.src === img1)? img2 : img1;
